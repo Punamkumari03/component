eec977be34f7448acd3f221eaa50ef3015f86efe
Punamkumari03 <punamgupta250602@gmail.com>
2023-04-19 13:08:37 +0530

    testimonial component

---
+++ src/component/Testimonial.jsx
@@ -0,0 +1,38 @@
+import React from "react";
+import "../index.css";
+
+function Testimonial() {
+  return (
+    <div className="testimonials">
+      <div className="container">
+        <h1>Testimonials</h1>
+       
+        <div className="row">
+          <div className="col-md-4 text-center">
+            <div className="profile">
+                <img src="https://images.pexels.com/photos/7090350/pexels-photo-7090350.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load" alt=""className="user"/>
+                <blockquote>The history of the labor movement needs to be taught in every school in this land. America is a living testimonial to what free men and women, organized in free democratic trade unions can do to make a better life. We ought to be proud of it!</blockquote>
+                <h3>punam Gupta <span>Co-Founder at XYX</span></h3>
+            </div>
+          </div>
+          <div className="col-md-4 text-center">
+            <div className="profile">
+                <img src="https://images.pexels.com/photos/7090350/pexels-photo-7090350.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load" alt=""className="user"/>
+                <blockquote>The history of the labor movement needs to be taught in every school in this land. America is a living testimonial to what free men and women, organized in free democratic trade unions can do to make a better life. We ought to be proud of it!</blockquote>
+                <h3>punam Gupta <span>Co-Founder at XYX</span></h3>
+            </div>
+          </div>
+          <div className="col-md-4 text-center">
+            <div className="profile">
+                <img src="https://images.pexels.com/photos/7090350/pexels-photo-7090350.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load" alt=""className="user"/>
+                <blockquote>The history of the labor movement needs to be taught in every school in this land. America is a living testimonial to what free men and women, organized in free democratic trade unions can do to make a better life. We ought to be proud of it!</blockquote>
+                <h3>punam Gupta <span>Co-Founder at XYX</span></h3>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Testimonial;
